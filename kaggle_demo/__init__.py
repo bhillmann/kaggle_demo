31fd1950a535cb8e79e26c19fbdcb22076c0a0b0
bhillmann <bhillmann@outlook.com>
2014-07-14 23:30:11 -0500

    Second example commit
    Added more files

---
+++ kaggle_demo/__init__.py
@@ -0,0 +1,1 @@
+__author__ = 'Benjamin Hillmann <bhillmann@outlook.com>'
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='kaggle_demo',
+    version='0.0.0',
+    packages=['kaggle_demo'],
+    url='',
+    license='',
+    author='Benjamin Hillmann',
+    author_email='bhillmann@outlook.com',
+    description='Example set up for python packaging'
+)
